58ba1695dc56941973433dd96da37198010d187c
calixlock <calixlock@naver.com>
2024-05-03 23:55:28 +0900

    vue05 +btn implement

--- rocksEasyWeb/vuejs/vue-project-000/src/views/ReactivityView.vue
+++ rocksEasyWeb/vuejs/vue-project-000/src/views/ReactivityView.vue
@@ -1,9 +1,17 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+console.log(count);
+console.log(count.value);
+count.value++;
+console.log(count.value);
+</script>
 <template>
   <div class="layout">
     <h1>This is an Reactivity page</h1>
-
     <hr />
+    <button @click="count++">+</button>
+    <div>{{ count }}</div>
   </div>
 </template>
 
