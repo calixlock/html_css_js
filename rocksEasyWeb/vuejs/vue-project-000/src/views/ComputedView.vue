9bfe6d74241a1e14b878e45dec5164a3861febdd
calixlock <calixlock@naver.com>
2024-05-07 17:16:39 +0900

    vue06 reactive style > change computed method

--- rocksEasyWeb/vuejs/vue-project-000/src/views/ComputedView.vue
+++ rocksEasyWeb/vuejs/vue-project-000/src/views/ComputedView.vue
@@ -18,17 +18,36 @@ export default {
     messageBooks() {
       return this.author.books.length;
     },
+    fullName: {
+      get() {
+        return this.lastName + " " + this.firstName;
+      },
+      set(newValue) {
+        // es6 구조분해 할당
+        [this.lastName, this.firstName] = newValue.split(" ");
+      },
+    },
   },
 };
+console.log(this);
 </script>
 <template>
   <div class="layout">
     <h1>This is an computed properties page</h1>
     <hr />
-    <!-- computed 방식 -->
     {{ author }}
     <hr />
+    <!-- computed 방식 -->
     <span>책장 : {{ messageBooks }}</span>
+    <hr />
+    <!-- fullName.get 실행 -->
+
+    <div>수정 : {{ (this.fullName = "홍 길동") }}</div>
+    <div>lastName : {{ this.lastName }}</div>
+    <div>firstName : {{ this.firstName }}</div>
+    <hr />
+    <!-- check -->
+    <div>{{}}</div>
   </div>
 </template>
 
