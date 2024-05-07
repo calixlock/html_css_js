5639a93f478fd97c31a3e9e37ca0e41ae5a3c0b4
calixlock <calixlock@naver.com>
2024-05-07 17:43:34 +0900

    vue07 v-if/else-if/else

--- rocksEasyWeb/vuejs/vue-project-000/src/views/ConditionalRenderView.vue
+++ rocksEasyWeb/vuejs/vue-project-000/src/views/ConditionalRenderView.vue
@@ -1,12 +1,26 @@
-<script setup>
-const awesome = true;
+<script>
+export default {
+  // data 초기값 설정
+  data() {
+    return {
+      awesome: true,
+      type: "d",
+    };
+  },
+};
 </script>
 <template>
   <div class="layout">
     <h1>This is an conditionalRender page</h1>
     <hr />
     <button @click="awesome = !awesome">전환</button>
-    {{ awesome }}
+    <h2 v-if="awesome">awesome : {{ awesome }}</h2>
+    <h2 v-else>awesome : {{ awesome }}</h2>
+    <hr />
+    <h2 v-if="type === 'a'">{{ type }} : type</h2>
+    <h2 v-else-if="type === 'b'">{{ type }} : type</h2>
+    <h2 v-else-if="type === 'c'">{{ type }} : type</h2>
+    <h2 v-else>{{ type }} : type</h2>
   </div>
 </template>
 
