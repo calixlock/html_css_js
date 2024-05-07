983672ff5653d264bdf1762b3ffa8796b65f7d55
calixlock <calixlock@naver.com>
2024-05-07 16:45:09 +0900

    vue06 computed reactive execute

--- rocksEasyWeb/vuejs/vue-project-000/src/views/ComputedView.vue
+++ rocksEasyWeb/vuejs/vue-project-000/src/views/ComputedView.vue
@@ -1,8 +1,21 @@
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+const author = reactive({
+  name: "John Doe",
+  books: [
+    "Vue 2 - Advanced Guide",
+    "Vue 3 - Basic Guide",
+    "Vue 4 - The Mystery",
+  ],
+});
+</script>
 <template>
   <div class="layout">
     <h1>This is an computed properties page</h1>
     <hr />
+    {{ author }}
+    <hr />
+    {{ author.books.length }}
   </div>
 </template>
 
