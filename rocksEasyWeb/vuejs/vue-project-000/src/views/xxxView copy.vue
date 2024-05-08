71749c5b72348aaabd762e3fd2351926b5a3c88d
calixlock <calixlock@naver.com>
2024-05-08 23:55:51 +0900

    vue08 listRendering update

---
+++ rocksEasyWeb/vuejs/vue-project-000/src/views/xxxView copy.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+<template>
+  <div class="layout">
+    <h1>This is an xxx page</h1>
+    <hr />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
