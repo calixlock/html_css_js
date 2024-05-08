44cead56ca2e9ca07969f00869e60f1796ddbde6
calixlock <calixlock@naver.com>
2024-05-09 02:35:44 +0900

    vue08 v-for list render

--- rocksEasyWeb/vuejs/vue-project-000/src/views/ListRenderingView.vue
+++ rocksEasyWeb/vuejs/vue-project-000/src/views/ListRenderingView.vue
@@ -1,7 +1,14 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      parentLangs: "Web",
+      items: [
+        { id: 1, title: "HTML", desc: "HTML..." },
+        { id: 2, title: "CSS", desc: "CSS..." },
+        { id: 3, title: "Javascript", desc: "Javascript..." },
+      ],
+    };
   },
 };
 </script>
@@ -9,6 +16,11 @@ export default {
   <div class="layout">
     <h1>This is an List Rendering page</h1>
     <hr />
+    <ul>
+      <li v-for="(item, idx) in items" :key="item.id">
+        {{ parentLangs }} - {{ idx }} - {{ item.title }} - {{ item.desc }}
+      </li>
+    </ul>
   </div>
 </template>
 
