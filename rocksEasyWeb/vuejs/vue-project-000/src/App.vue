d05b8a72682e423ed200674ecdf3a3d4eb04af8a
calixlock <calixlock@naver.com>
2024-05-08 23:59:00 +0900

    .

--- rocksEasyWeb/vuejs/vue-project-000/src/App.vue
+++ rocksEasyWeb/vuejs/vue-project-000/src/App.vue
@@ -21,7 +21,7 @@ import HelloWorld from "./components/HelloWorld.vue";
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/template">Template Grammer</RouterLink>
         <RouterLink to="/reactivity">Reactivity</RouterLink>
-        <RouterLink to="/Computed">Computed</RouterLink>
+        <RouterLink to="/computed">Computed</RouterLink>
         <RouterLink to="/conditionalRender">ConditionalRender</RouterLink>
         <RouterLink to="/listRender">ListRender</RouterLink>
       </nav>
