c6b19ab621d3931b066a42aa49419f8e3233a203
calixlock <calixlock@naver.com>
2024-05-10 00:14:31 +0900

    vue08 .

--- rocksEasyWeb/vuejs/vue-project-000/src/views/ListRenderingView.vue
+++ rocksEasyWeb/vuejs/vue-project-000/src/views/ListRenderingView.vue
@@ -14,12 +14,21 @@ export default {
         publishedAt: "2016-04-10",
       },
       numbers: [1, 2, 3, 4, 5],
+      sets: [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+      ],
     };
   },
-  // evenNumbers는 numbers의  data가 변경되었을 때 실행됨
+  // evenNumbers는 numbers의 data가 변경되었을 때 실행됨
   computed: {
     evenNumbers() {
       return this.numbers.filter((n) => n % 2 === 0);
+    },
+  },
+  methods: {
+    even(numbers) {
+      return numbers.filter((numbers) => numbers % 2 === 0);
     },
   },
 };
@@ -28,26 +37,31 @@ export default {
   <div class="layout">
     <h1>This is an List Rendering page</h1>
     <hr />
+    <h2>v-for : basic render</h2>
     <ul>
       <li v-for="(item, idx) in items" :key="item.id">
         {{ parentLangs }} - {{ idx }} - {{ item.title }} - {{ item.desc }}
       </li>
     </ul>
     <hr />
+    <h2>v-for : data list render</h2>
     <ul>
       <li v-for="(value, idx) in myObject" :key="value">
         {{ idx }} : {{ value }}
       </li>
     </ul>
     <hr />
+    <h2>v-for numbering render</h2>
     <span v-for="n in 10" :key="n">{{ n }}</span>
     <hr />
+    <h2>v-for template tag render</h2>
     <ul>
-      <template v-for="item in items" :key="item.id">
-        <li>{{ item.desc }}</li>
+      <template v-for="(item, idx) in items" :key="item.id">
+        <li>{{ idx }} - {{ item.desc }}</li>
       </template>
     </ul>
     <hr />
+    <h2>v-for computed data render</h2>
     <ul>
       <li v-for="n in evenNumbers" :key="n">{{ n }}</li>
     </ul>
