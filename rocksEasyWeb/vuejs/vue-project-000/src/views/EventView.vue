bcede0538137e43b4ef8d00c0bbd88a6fb36003a
calixlock <calixlock@naver.com>
2024-05-10 19:20:21 +0900

    vue09 eventHandler inline/method

--- rocksEasyWeb/vuejs/vue-project-000/src/views/EventView.vue
+++ rocksEasyWeb/vuejs/vue-project-000/src/views/EventView.vue
@@ -3,7 +3,22 @@ export default {
   data() {
     return {
       count: 0,
+      name: "ahn",
     };
+  },
+  methods: {
+    greet(event) {
+      // this는 메서드가 활성화된 현재의 인스턴스(script>default)
+      alert(`hello, ${this.name}`);
+      if (event) {
+        alert(event.target.tagName);
+        console.log(event);
+      }
+    },
+    say(msg) {
+      // console.log(msg);
+      alert(msg);
+    },
   },
 };
 </script>
@@ -11,8 +26,15 @@ export default {
   <div class="layout">
     <h1>This is an Event page</h1>
     <hr />
-    <h2>inline handler</h2>
+    <h2>Inline handler</h2>
     <button @click="count++">click : {{ count }}</button>
+    <hr />
+    <h2>Method handler</h2>
+    <button @click="greet">Hello</button>
+    <hr />
+    <h2>inline > method handler > call</h2>
+    <button @click="say(name)">hello</button>
+    <button @click="say('bye')">bye</button>
   </div>
 </template>
 
