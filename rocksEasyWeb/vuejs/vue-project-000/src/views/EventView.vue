d13357e5a9e1432032eefd2c7e721430d6cde55c
calixlock <calixlock@naver.com>
2024-05-11 23:34:33 +0900

    vue09 Event Modifier

--- rocksEasyWeb/vuejs/vue-project-000/src/views/EventView.vue
+++ rocksEasyWeb/vuejs/vue-project-000/src/views/EventView.vue
@@ -26,12 +26,30 @@ export default {
       }
       alert(msg);
     },
+    doThis() {
+      //.stop
+      alert("링크 전파를 차단했습니다.");
+    },
+    onSubmit() {
+      // .onSubmit
+      alert("폼 제출을 막습니다.");
+    },
+    doThat() {
+      // .stop.prevent
+      alert("기본 속성 차단 / 링크 전파 차단");
+    },
+    boxClick() {
+      alert("boxClick");
+    },
+    spanClick() {
+      alert("spanClick");
+    },
   },
 };
 </script>
 <template>
   <div class="layout">
-    <h1>This is an Event page</h1>
+    <h1>This is an Event handlerpage</h1>
     <hr />
     <h2>Inline handler</h2>
     <button @click="count++">click : {{ count }}</button>
@@ -49,10 +67,45 @@ export default {
       submit_2
     </button>
     <hr />
+    <h2>Event Modifier</h2>
+    <!-- the click event's propagation will be stopped -->
+    <!-- 클릭 이벤트 부모에게 전달되는거 중지 / 본인 이벤트 실행 / target="_blank" / 새창에서 열리도록-->
+    <a @click.stop="doThis" href="http://www.naver.com" target="_blank"
+      >doThis</a
+    >
+
+    <!-- the submit event will no longer reload the page / action 동작 중지-->
+    <form @submit.prevent="onSubmit" action="http://www.naver.com">
+      <button>submit</button>
+    </form>
+
+    <!-- modifiers can be chained / 기본 이벤트 차단 및 이벤트 전달 둘다 중지 -->
+    <a @click.stop.prevent="doThat">stop & prevent</a>
+
+    <!-- just the modifier  / 기본기능 중지-->
+    <form @submit.prevent action="http://www.naver.com">
+      <button>submit.prevent</button>
+    </form>
+
+    <!-- span click 시 box도 포함되어 둘다 실행됨  -->
+    <div @click="boxClick" class="box">
+      <span @click="spanClick" class="box">spanClick</span>
+    </div>
+
+    <!-- span / box의 클릭 범위(영역) 나눠짐 구분됨 -->
+    <div @click.self="boxClick" class="box">
+      <span @click.self="spanClick" class="box">spanClick.self</span>
+    </div>
   </div>
 </template>
 
 <style>
+.box {
+  border: 1px solid;
+  padding: 20px;
+  display: inline-block;
+}
+
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
