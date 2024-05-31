d65e2af33b86ce146ededd1b3dcc9bc884772aa1
calixlock <calixlock@naver.com>
2024-05-31 17:06:21 +0900

    vue11 lifeCycle function update

--- rocksEasyWeb/vuejs/vue-project-000/src/views/LifecycleView.vue
+++ rocksEasyWeb/vuejs/vue-project-000/src/views/LifecycleView.vue
@@ -1,11 +1,33 @@
 <script>
-export default {};
+export default {
+  mounted() {
+    console.log(`3: 컴포넌트 마운트 되었습니다.`);
+  },
+  beforeMount() {
+    console.log(`0: beforeMount 실행`);
+  },
+  beforeUpdate() {
+    console.log(`1-0: update 전 실행`);
+  },
+  updated() {
+    console.log(`2-1: update 후 실행`);
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
 </script>
 <template>
+  {{ console.log(`1: 템플릿 컴파일 시작`) }}
   <div class="layout">
     <h1>This is an lifecycle hooks page</h1>
     <hr />
+    <button @click="count++">+</button>
+    <div id="counter">count : {{ count }}</div>
   </div>
+  {{ console.log(`2: 템플릿 컴파일 완료`) }}
 </template>
 
 <style>
