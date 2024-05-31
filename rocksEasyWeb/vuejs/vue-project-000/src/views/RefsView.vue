ac5ab2118b9df06d1fa75691d3c688588c0ac340
calixlock <calixlock@naver.com>
2024-05-31 18:09:24 +0900

    vue13 ref utilize method

--- rocksEasyWeb/vuejs/vue-project-000/src/views/RefsView.vue
+++ rocksEasyWeb/vuejs/vue-project-000/src/views/RefsView.vue
@@ -1,9 +1,15 @@
 <script>
 export default {
+  data() {
+    return {
+      list: [11, 22, 33, 44],
+    };
+  },
   mounted() {
     // mounted가 되면 ref가 input인 곳에 focus를 해라
     // this.$refs.input.focus();
-    document.querySelector("#username").focus();
+    // document.querySelector("#username").focus();
+    console.log(this.$refs.items);
   },
 };
 </script>
@@ -12,7 +18,10 @@ export default {
     <h1>This is refs page</h1>
     <hr />
     <!-- <input ref="input" /> -->
-    <input type="text" id="username" />
+    <!-- <input type="text" id="username" /> -->
+    <ul>
+      <li v-for="item in list" ref="items" :key="item.id">{{ item }}</li>
+    </ul>
   </div>
 </template>
 
