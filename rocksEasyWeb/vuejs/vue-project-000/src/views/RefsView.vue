1782c39aa0d0ab95aaa4afdf67365737e7e56557
calixlock <calixlock@naver.com>
2024-05-31 17:58:46 +0900

    vue13 input tag focus method 01

--- rocksEasyWeb/vuejs/vue-project-000/src/views/RefsView.vue
+++ rocksEasyWeb/vuejs/vue-project-000/src/views/RefsView.vue
@@ -1,8 +1,16 @@
-<script setup></script>
+<script>
+export default {
+  mounted() {
+    // mounted가 되면 ref가 input인 곳에 focus를 해라
+    this.$refs.input.focus();
+  },
+};
+</script>
 <template>
   <div class="layout">
     <h1>This is refs page</h1>
     <hr />
+    <input ref="input" />
   </div>
 </template>
 
