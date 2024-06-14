6593e4ee50f0d8134669f52349fcd0d422699716
calixlock <calixlock@naver.com>
2024-06-14 12:33:24 +0900

    manifest update

--- chromeExtension/01_desc_manifest.md
+++ chromeExtension/01_desc_manifest.md
@@ -1,27 +1,3 @@
-### status
-
-- 2024.06.11 ~
-- 4 / 9
-
-### 참고
-
-#### 참고 영상
-
-- https://www.youtube.com/playlist?list=PLAaf3-EPzrYPpyeIsQ2GGqvMqaahMJzqU
-- ***
-
-### Docs
-
-#### chrome extension
-
-- https://developer.chrome.com/docs/extensions/whats-new?hl=ko
-
-#### content script
-
-- https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts?hl=ko
-
----
-
 ### manifest.json
 
 ```json
--- chromeExtension/codingLife_Frequency/background.js
+++ chromeExtension/codingLife_Frequency/background.js
@@ -1,39 +1,6 @@
-// content page의 모든 body의 text 가져오기
-// let bodyText = document.querySelector('body').innerText;
-
-// 크롬확장에서 tabs와 관련된 기능 중에 컨텐츠 페이지를 대상으로 코드를 실행
-// chrome.tabs.executeScript는 manifest v2에서 사용 되던 api 임으로 동작x
-// chrome.tabs.executeScript(
-//   {
-//     code: 'document.querySelector("body").innerText',
-//   },
-//   function (result) {
-//     //위 의 code:~~~ 가 실행된 후 이 함수를 실행하여 result에 그 결과를 연결
-//     alert(result);
-//   }
-// );
-
-// 백그라운드 스크립트에서 chrome.scripting.executeScript를 사용하여 content script를 실행
-// chrome.action.onClicked.addListener((tab) => {
-//   chrome.scripting.executeScript(
-//     {
-//       target: { tabId: tab.id },
-//       func: () => document.body.innerText,
-//     },
-//     (results) => {
-//       if (results && results[0] && results[0].result) {
-//         console.log(results[0].result);
-//       }
-//     }
-//   );
-// });
+// popup창에서 실행됨 / action보다 먼저 실행됨
 console.log("background_Js 실행");
 
 let color = "#3aa757";
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log("Default background color set to %cgreen", `color: ${color}`);
-});
 
 console.log(color);
